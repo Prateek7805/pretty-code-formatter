9e037925b14b928356007af049a0445885fb35a4
Prateek7805 <prateekpjimmy@gmail.com>
2023-12-02 23:28:06 +0530

    fixed ui issue in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,17 +4,17 @@ import Flower from '../Icons/Flower/Flower';
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
 import LanguageSelector from '../LanguageSelector/LanguageSelector';
 import RunButton from '../Buttons/RunButton/RunButton';
-export default function Navbar(){
+export default function Navbar() {
     return (
         <div className="row g-0 navbar-custom">
-            <div className='col-8 d-flex flex-row gap-2 px-5 align-items-center'>
-            <Flower/>
-            <p className='nav-title'>Auto Code Prettier</p>
+            <div className='col-8 d-flex flex-row gap-2 px-3 px-md-5 align-items-center'>
+                <Flower />
+                <p className='nav-title'>Pretty Code</p>
             </div>
             <div className='col-4 d-flex flex-row justify-content-end px-2 align-items-center gap-1'>
-                <RunButton/>
-                <LanguageSelector/>
-                <ThemeToggle/>
+                <RunButton />
+                <LanguageSelector />
+                <ThemeToggle />
             </div>
         </div>
     )
