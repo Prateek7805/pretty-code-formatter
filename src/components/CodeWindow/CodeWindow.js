25268b14ef591e4c3f82774eb0630b813534b2f8
Prateek7805 <prateekpjimmy@gmail.com>
2023-12-03 19:27:47 +0530

    copy fix in mobile devices

--- src/components/CodeWindow/CodeWindow.js
+++ src/components/CodeWindow/CodeWindow.js
@@ -7,20 +7,27 @@ import { useTheme } from '@emotion/react';
 import { CodeData, ModalData } from '../Contexts/PageContext';
 import CopyTextButton from '../Buttons/CopyTextButton/CopyTextButton';
 
-export default function CodeWindow(){
+export default function CodeWindow() {
     const theme = useTheme();
-    const {code} = useContext(CodeData);
-    const {setMData} = useContext(ModalData);
-    const codeTheme = theme.palette.mode === "light"? vs : vscDarkPlus;
-    const copyTextToClipBoard = async () => {
+    const { code } = useContext(CodeData);
+    const { setMData } = useContext(ModalData);
+    const codeTheme = theme.palette.mode === "light" ? vs : vscDarkPlus;
+    const copyTextToClipBoard = () => {
+
+        const formattedCode = code.formatted;
+        if (formattedCode === '' || formattedCode === undefined) {
+            return;
+        }
+        const textarea = document.createElement('textarea');
+        textarea.value = code.formatted;
+        textarea.style.position = 'absolute';
+        textarea.style.left = '-9999px';
+        document.body.appendChild(textarea);
+        textarea.select();
         try {
-            const formattedCode = code.formatted;
-            if(formattedCode === '' || formattedCode === undefined){
-                return;
-            }
-            await navigator.clipboard.writeText(formattedCode);
-        }catch(error){
-            setMData(prev=>({
+            document.execCommand('copy');
+        } catch (error) {
+            setMData(prev => ({
                 ...prev,
                 alertModal: {
                     ...prev.alertModal,
@@ -30,18 +37,21 @@ export default function CodeWindow(){
                 }
             }));
         }
+        finally {
+            document.body.removeChild(textarea);
+        }
     }
     return (
-            <div className='codeblock-wrapper'>
-                <CopyTextButton onClick={copyTextToClipBoard}/>
-            <SyntaxHighlighter 
+        <div className='codeblock-wrapper'>
+            <CopyTextButton onClick={copyTextToClipBoard} />
+            <SyntaxHighlighter
                 className='codeblock-window'
                 showLineNumbers={true}
-                language={code.highlighted.toLowerCase()} 
+                language={code.highlighted.toLowerCase()}
                 style={codeTheme}
-                >
+            >
                 {code.formatted}
             </SyntaxHighlighter>
-            </div>
+        </div>
     );
 };
